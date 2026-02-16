0b00fd53cda1e86a417dea44d6ce07a7a97f8e65
Han Olff <h.olff@rug.nl>
2026-02-16 23:22:15 +0100

    glm

--- scripts/13-generalized-linear-mixed-models.R
+++ scripts/13-generalized-linear-mixed-models.R
@@ -30,6 +30,7 @@ levels(JK_vegetation$FactSpecRich$graztreat)<-c("vole","vole+rabbit","vole+rabbi
 ggplot(data=JK_vegetation$FactSpecRich,aes(x=graztreat,y=specrich,fill=graztreat)) +
   geom_boxplot()
 
+hist(JK_vegetation$FactSpecRich$specrich)
 # explore the distribution of the species richness data within each treatment
 JK_vegetation$FactSpecRich |> 
   ggplot(aes(x=specrich)) +
@@ -41,13 +42,20 @@ JK_vegetation$FactSpecRich |>
 # test with  linear model assuming the residuals are normally distributed,
 # and inspect the Q-Q plot of the residuals, and do a Shapiro test for normality 
 # do a one-way anova ( 1 predictor with >2 categories)
-
-
+m1<-lm(specrich~graztreat,
+       data=JK_vegetation$FactSpecRich)
+summary(m1)
 # show the table of the results of the one-way analysis of variance
+stats::anova(m1,test="F")
 
 # test which differences between treatments are significant
+emm <- emmeans::emmeans(m1, ~graztreat)
+pairs(emm,adjust="tukey")
+# letters: a a b
+# means with the same letter are not significantly differently
 
 # explore the q-q plot of the residuals of the model to check normality
+plot(m1,which=2)
 
 # plot the histogram of the residuals with normal curve
 g=m1$residuals
@@ -61,17 +69,27 @@ shapiro.test(g) # test if significant different from a normal distribution
 
 #--------------------04 explore the results with a generalized linear mixed model
 # do a glmmTMB assuming normally distributed residuals (so similar to lm)
+m2<-glmmTMB::glmmTMB(specrich~graztreat,
+                     family=gaussian(link="identity"),
+                     data=JK_vegetation$FactSpecRich)
+car::Anova(m2,test="Chisq")
 
 
 # but the response variable is not continuous - only integers
 # test with generalized linear model assuming now a poisson distribution of residuals 
 # if plant species richness is different between the treatments 
+m2<-glmmTMB::glmmTMB(specrich~graztreat,
+                     family=poisson(link="log"),
+                     data=JK_vegetation$FactSpecRich)
+car::Anova(m2,test="Chisq")
 
 # compare the models with AIC test, when more then 2 lower, then better model
+AIC(m2,m1)
 
 # pairwaise comparisons, not that Tukey test is not valid for this model
 
-
+emm <- emmeans::emmeans(m2, ~graztreat)
+pairs(emm,adjust="tukey")
 
 # explore the effect of blocks on the species richness
 # plot cover mean species richness with the random factor
@@ -89,25 +107,49 @@ ggplot(aes(x=block,y=specrich,fill=graztreat),
 # block is a fixed effect
 # this is not correct because block is a random effect, requiring a mixed model 
 
-
+m3<-glmmTMB::glmmTMB(specrich~graztreat + block,
+                     family=poisson(link="log"),
+                     data=JK_vegetation$FactSpecRich)
+car::Anova(m3,test="Chisq")
 
 # test with  linear mixed model (assuming normal error  distribution) 
 # if plant species richness is different between the treatments 
 # and account for block effects in the design, assuming it is a random effect
 # first fit a model with only block, assuming poisson distribution
-
+m4<-glmmTMB::glmmTMB(specrich ~ (1|block),
+                     family=poisson(link="log"),
+                     data=JK_vegetation$FactSpecRich)
+summary(m4)
+car::Anova(m4,test="Chisq")
 
 # random intercept, fixed slopes model, assuming the treatment effect is the same for every block
+m5<-glmmTMB::glmmTMB(specrich ~ graztreat + (1|block),
+                     family=poisson(link="log"),
+                     data=JK_vegetation$FactSpecRich)
+summary(m5)
+car::Anova(m5,test="Chisq")
 
 
 
 # check if this model explains more variation, at least 2 units lower AIC
-
+AIC(m5,m4)
 
 # random intercept, random slopes model, assume the treatment effect is different per block
+# so the slope of the treatment effect is allowed to vary per block
+
+m6<-glmmTMB::glmmTMB(specrich ~ graztreat + (1|block) + (1 + (graztreat|block)),
+                     family=poisson(link="log"),
+                     data=JK_vegetation$FactSpecRich)
+summary(m6)
+car::Anova(m6,test="Chisq")
 
 
 # drop the fitting of covariances between the parameters in the model
+m7<-glmmTMB::glmmTMB(specrich ~ graztreat + (1|block) + (0 + (graztreat|block)),
+                     family=poisson(link="log"),
+                     data=JK_vegetation$FactSpecRich)
+summary(m7)
+car::Anova(m7,test="Chisq")
 
 
 # check if better than the previous
